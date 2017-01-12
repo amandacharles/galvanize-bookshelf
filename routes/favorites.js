df99c66598c0fe16f35fc907a7fbc8a7a45f0311
Amanda Charles <ancharles11@gmail.com>
2017-01-12 09:52:09 -0800

    favorites fi

--- routes/favorites.js
+++ routes/favorites.js
@@ -32,22 +32,22 @@ router.get('/favorites', authorize, (req, res, next) => {
     });
 });
 
-router.get('/favorites/check?bookId=1', authorize, (req, res, next) => {
-  let theId = req.query.bookId;
-
-  knex('favorites')
-    .innerJoin('books', 'books.id', 'favorites.book_id')
-    .where('favorites.user_id', req.claim.userId)
-    .where(theId == books.id)
-    .then((rows) => {
-      const favorites = camelizeKeys(rows);
-
-      res.send(favorites);
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
+// router.get('/favorites/check?bookId=1', authorize, (req, res, next) => {
+//   let theId = req.query.bookId;
+//
+//   knex('favorites')
+//     .innerJoin('books', 'books.id', 'favorites.book_id')
+//     .where('favorites.user_id', req.claim.userId)
+//     .where(theId == books.id)
+//     .then((rows) => {
+//       const favorites = camelizeKeys(rows);
+//
+//       res.send(favorites);
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// });
 
 
 module.exports = router;
